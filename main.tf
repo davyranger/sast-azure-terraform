4a373824f8805558262d0a7e8f68eb99f12833fd
Davy <davy.strain@hotmail.com>
2024-09-17 19:53:08 +0800

    pushing to feature branch to test pipeline

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ provider "azurerm" {
   use_oidc = true
 }
 
-# Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
+## Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
   location = var.location
